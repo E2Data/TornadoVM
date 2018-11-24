0ed7b97be644e971304f79f22d9db893693450ca
Mary <maria.xekalaki@postgrad.manchester.ac.uk>
2018-11-24 17:06:46 +0000

    <test>: Map-Reduce implementation of KMeans on Tornado

--- examples/src/main/java/uk/ac/manchester/tornado/examples/TestFlink.java
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/TestFlink.java
@@ -3,49 +3,241 @@ package uk.ac.manchester.tornado.examples;
 import uk.ac.manchester.tornado.api.TaskSchedule;
 import uk.ac.manchester.tornado.api.TornadoDriver;
 import uk.ac.manchester.tornado.api.annotations.Parallel;
+import uk.ac.manchester.tornado.api.annotations.Reduce;
 import uk.ac.manchester.tornado.api.enums.TornadoDeviceType;
 import uk.ac.manchester.tornado.api.runtime.TornadoRuntime;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class TestFlink {
 
-    // public static final double[] centroids_id = new double[] { 1, 2, 3 };
-    // public static final double[] centroids_x = new double[] { -31.85f, 35.16f,
-    // 2.3f };
-    // public static final double[] centroids_y = new double[] { -44.77f, 17.46f,
-    // 3.1f };
+    static double[] pointsx;
+    static double[] pointsy;
 
-    // public static final double[] points_x = new double[] { -14.22f, 62.78f,
-    // 76.18f, 35.04f, -9.53f, -34.35f, 1.2f, 1.3, -8.52, 6.5 };
-    // public static final double[] points_y = new double[] { -48.01f, 57.10f,
-    // 56.18f, -42.99f, 50.29f, -46.26f, 3.4f, 4.3, 48.21, 7.2 };
+    static double[] centroidsID = new double[2];
+    static double[] centroidsX = new double[2];
+    static double[] centroidsY = new double[2];
 
-    public static final double[] p_x = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-    // public static final double[] p_y = new double[] { 1, 2, 4, 4, 5, 6, 7, 8, 9,
-    // 10 };
+    public static void readDataSetsFromFile(String fileName, int numOfpoints) {
 
-    /*
-     * public static double[] c_1(double[] point_x, double[] point_y, double[]
-     * accres) { double p1,p2,sumx,sumy; if (point_x.length == 1) { p1 = point_x[0];
-     * p2 = point_y[0]; sumx = p1; sumy = p2; } else { p1 = point_x[0] + point_x[1];
-     * p2 = point_y[0] + point_y[1]; sumx = p1; sumy = p2; } for (@Parallel int i =
-     * 2; i < point_x.length; i++) { p1 = sumx + point_x[i]; p2 = sumy + point_y[i];
-     * sumx = p1; sumy = p2; }
-     * 
-     * accres[0] = sumx; accres[1] = sumy; accres[2] = point_x.length; return
-     * accres; }
-     */
+        pointsx = new double[numOfpoints];
+        pointsy = new double[numOfpoints];
+        // This will reference one line at a time
+        String line;
+        boolean flag = false;
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            // flag is false while we are reading points
+            int i = 0;
+            int j = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (!line.contains("---- Centroids:") && !flag) {
+                    StringTokenizer multiTokenizer = new StringTokenizer(line, "{,}");
+                    String num1 = null;
+                    String num2 = null;
+                    while (multiTokenizer.hasMoreTokens()) {
+                        if (num1 == null) {
+                            num1 = multiTokenizer.nextToken();
+                        } else {
+                            num2 = multiTokenizer.nextToken();
+                        }
+                    }
+                    pointsx[i] = Double.parseDouble(num1);
+                    pointsy[i] = Double.parseDouble(num2);
+                    i++;
+                }
+                if (line.contains("---- Centroids:")) {
+                    // we have read all the points, now set flag to true to store the centroids
+                    flag = true;
+                    line = bufferedReader.readLine();
+                }
+
+                if (flag) {
+                    // read centroids
+                    StringTokenizer multiTokenizer = new StringTokenizer(line, "{,}");
+                    String id = null;
+                    String num1 = null;
+                    String num2 = null;
+                    while (multiTokenizer.hasMoreTokens()) {
+                        if (id == null) {
+                            id = multiTokenizer.nextToken();
+                        } else if (num1 == null) {
+                            num1 = multiTokenizer.nextToken();
+                        } else {
+                            num2 = multiTokenizer.nextToken();
+                        }
+                    }
+                    centroidsID[j] = Double.parseDouble(id);
+                    centroidsX[j] = Double.parseDouble(num1);
+                    centroidsY[j] = Double.parseDouble(num2);
+                    j++;
+                }
+            }
+            bufferedReader.close();
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public static void selectNearestCentroids(double[] pointsX, double[] pointsY, double[] centroidsId, double[] centroidsX, double[] centroidsY, double[] selId) {
+        for (@Parallel int j = 0; j < pointsX.length; j++) {
+            double pointX = pointsX[j];
+            double pointY = pointsY[j];
+            double minDistance = Double.MAX_VALUE;
+            double closestCentroidId = -1;
+            double currCentrId = centroidsId[0];
+            if (currCentrId == 1) {
+                double distance = Math.sqrt((pointX - centroidsX[0]) * (pointX - centroidsX[0]) + (pointY - centroidsY[0]) * (pointY - centroidsY[0]));
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    closestCentroidId = centroidsId[0];
+                }
+                currCentrId = 2;
+            }
+            if (currCentrId == 2) {
+                double distance = Math.sqrt((pointX - centroidsX[1]) * (pointX - centroidsX[1]) + (pointY - centroidsY[1]) * (pointY - centroidsY[1]));
+                if (distance < minDistance) {
+                    closestCentroidId = centroidsId[1];
+                }
+            }
+            selId[j] = closestCentroidId;
+        }
+    }
+
+    public static void groupBy(double[] pointsx, double[] pointsy, double[] selid, double[] pointx1, double[] pointy1, double[] pointx2, double[] pointy2) {
+
+        for (@Parallel int i = 0; i < pointsx.length; i++) {
+            if (selid[i] == 1) {
+                pointx1[i] = pointsx[i];
+                pointy1[i] = pointsy[i];
+            } else {
+                pointx2[i] = pointsx[i];
+                pointy2[i] = pointsy[i];
+            }
+        }
+    }
+
+    public static void cendroidAccum(double[] point, @Reduce double[] accres) {
+        accres[0] = 0;
+        for (@Parallel int k = 0; k < point.length; k++) {
+            accres[0] += point[k];
+        }
+    }
+
+    public static void centroidAvg(double[] redResX, double[] redResY, int[] redResApp, double[] centrX, double[] centrY) {
+        for (@Parallel int i = 0; i < redResX.length; i++) {
+            centrX[i] = (redResX[i] / redResApp[i]);
+            centrY[i] = (redResY[i] / redResApp[i]);
+        }
+    }
+
+    public static TornadoDeviceType getDefaultDeviceType() {
+        TornadoDriver driver = TornadoRuntime.getTornadoRuntime().getDriver(0);
+        return driver.getTypeDefaultDevice();
+    }
 
     public static void main(String[] args) {
-        double[] test = new double[4];
-        TestF tf = new TestF();
-        TornadoReduceFunctionBase accum = (TornadoReduceFunctionBase) tf.retAccum();
-        new TaskSchedule("s1").task("t1", accum::treduce, p_x, test).streamOut(test).execute();
-        System.out.println(Arrays.toString(test));
-        // System.out.println("test = " + test[0]);
+
+        int numOfpoints = Integer.parseInt(args[0]);
+        String filename = args[1];
+
+        readDataSetsFromFile(filename, numOfpoints);
+
+        // selectNearest
+        double[] selId = new double[numOfpoints];
+        double[] selId2 = new double[numOfpoints];
+        // groupBy
+        double[] pointx1 = new double[numOfpoints];
+        double[] pointy1 = new double[numOfpoints];
+        double[] pointx2 = new double[numOfpoints];
+        double[] pointy2 = new double[numOfpoints];
+        int[] count = new int[2];
+        // centrAcc
+        double[] redResX1 = null;
+        double[] redResY1 = null;
+        double[] redResX2 = null;
+        double[] redResY2 = null;
+        // avg
+        double[] redResX = new double[2];
+        double[] redResY = new double[2];
+        double[] centrX = new double[2];
+        double[] centrY = new double[2];
+
+        // ---------------------------
+
+        TaskSchedule ts0 = new TaskSchedule("s0").task("t0", TestFlink::selectNearestCentroids, pointsx, pointsy, centroidsID, centroidsX, centroidsY, selId).streamOut(selId)
+                .task("t1", TestFlink::groupBy, pointsx, pointsy, selId, pointx1, pointy1, pointx2, pointy2).streamOut(pointx1).streamOut(pointy1).streamOut(pointx2).streamOut(pointy2);
+        ts0.execute();
+
+        int numpoints1 = 0;
+        int numpoints2 = 0;
+        for (int i = 0; i < pointsx.length; i++) {
+            if (selId[i] == 1) {
+                numpoints1++;
+            } else {
+                numpoints2++;
+            }
+        }
+        count[0] = numpoints1;
+        count[1] = numpoints2;
+
+        int numGroups1 = 1;
+        int numGroups2 = 1;
+        int centrpoints1 = count[0];
+        int centrpoints2 = count[1];
+
+        // 256 = workgroup size!
+        if (centrpoints1 > 256) {
+            numGroups1 = centrpoints1 / 256;
+        }
+        if (centrpoints2 > 256) {
+            numGroups2 = centrpoints2 / 256;
+        }
+        TornadoDeviceType deviceType = getDefaultDeviceType();
+        switch (deviceType) {
+            case CPU:
+                redResX1 = new double[Runtime.getRuntime().availableProcessors()];
+                redResY1 = new double[Runtime.getRuntime().availableProcessors()];
+                redResX2 = new double[Runtime.getRuntime().availableProcessors()];
+                redResY2 = new double[Runtime.getRuntime().availableProcessors()];
+                break;
+            case DEFAULT:
+                break;
+            case GPU:
+                redResX1 = new double[numGroups1];
+                redResY1 = new double[numGroups1];
+                redResX2 = new double[numGroups2];
+                redResY2 = new double[numGroups2];
+                break;
+            default:
+                break;
+        }
+
+        TaskSchedule ts1 = new TaskSchedule("s1").task("t0", TestFlink::cendroidAccum, pointx1, redResX1).streamOut(redResX1).task("t1", TestFlink::cendroidAccum, pointy1, redResY1)
+                .streamOut(redResY1).task("t2", TestFlink::cendroidAccum, pointx2, redResX2).streamOut(redResX2).task("t3", TestFlink::cendroidAccum, pointy2, redResY2).streamOut(redResY2);
+        ts1.execute();
+
+        for (int j = 0; j < redResX1.length; j++) {
+            redResX[0] += redResX1[j];
+            redResY[0] += redResY1[j];
+        }
+        for (int j = 0; j < redResX1.length; j++) {
+            redResX[1] += redResX2[j];
+            redResY[1] += redResY2[j];
+        }
+
+        TaskSchedule ts2 = new TaskSchedule("s2").task("t0", TestFlink::centroidAvg, redResX, redResY, count, centrX, centrY).streamOut(centrX).streamOut(centrY)
+                .task("t1", TestFlink::selectNearestCentroids, pointsx, pointsy, centroidsID, centrX, centrY, selId2).streamOut(selId2);
+        ts2.execute();
 
     }
 
