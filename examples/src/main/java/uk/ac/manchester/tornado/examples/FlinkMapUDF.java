648a003ae6fdbf733fbffa5adf8a4fac5b41dc2d
MaryXek <xekalaki.mary@gmail.com>
2019-10-03 11:41:28 +0100

    [feat] Examine the UDF types to determine if they are POJOs. At the moment, if the input and/or return types of the user function are objects, the function is executed as a regular java program, it does not go through Tornado

--- examples/src/main/java/uk/ac/manchester/tornado/examples/FlinkMapUDF.java
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/FlinkMapUDF.java
@@ -2,12 +2,16 @@ package uk.ac.manchester.tornado.examples;
 
 public class FlinkMapUDF {
 
-    public static final class Flink implements MapFunction<Integer, Integer> {
+    public static final class Flink implements MapFunction<Point, Centroid> {
 
         @Override
-        public Integer map(Integer value) {
-            int x = value - 2 + 9;
-            return value * x;
+        public Centroid map(Point value) {
+            // int x = value - 2 + 9;
+            // return x;
+            int id = value.x;
+            int x = value.x + 2 - 9;
+            int y = value.y - 2 + 9;
+            return new Centroid(id, x, y);
         }
 
     }
---
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/MiddleMap.java
@@ -0,0 +1,41 @@
+package uk.ac.manchester.tornado.examples;
+
+public abstract class MiddleMap<T, O> {
+
+    // name composes of mymap + returntype + arg type
+
+    public abstract int mymapintint(int i);
+
+    public abstract int mymapintdouble(double i);
+
+    public abstract int mymapintfloat(float i);
+
+    public abstract int mymapintlong(long i);
+
+    public abstract double mymapdoubledouble(double i);
+
+    public abstract double mymapdoubleint(int i);
+
+    public abstract double mymapdoublefloat(float i);
+
+    public abstract double mymapdoublelong(long i);
+
+    public abstract float mymapfloatfloat(float i);
+
+    public abstract float mymapfloatint(int i);
+
+    public abstract float mymapfloatdouble(double i);
+
+    public abstract float mymapfloatlong(long i);
+
+    public abstract long mymaplonglong(long i);
+
+    public abstract long mymaplongint(int i);
+
+    public abstract long mymaplongdouble(double i);
+
+    public abstract long mymaplongfloat(float i);
+
+    public abstract O mymapobjectobject(T o);
+
+}
