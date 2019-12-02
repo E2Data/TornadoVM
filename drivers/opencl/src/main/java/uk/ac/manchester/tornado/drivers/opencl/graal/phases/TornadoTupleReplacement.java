6c6a019f1de3edcc37c84e867c80990634bcd2cd
MaryXek <xekalaki.mary@gmail.com>
2019-12-02 18:29:41 +0000

    [WIP] Create a new compiler phase to support Tuples on Tornado Change the JavaKind of the LoadIndexed and StoreIndexed nodes from "Object" to the raw type of the Tuple field

--- drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/graal/phases/TornadoTupleReplacement.java
+++ drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/graal/phases/TornadoTupleReplacement.java
@@ -1,27 +1,16 @@
 package uk.ac.manchester.tornado.drivers.opencl.graal.phases;
 
 import jdk.vm.ci.meta.Constant;
-import jdk.vm.ci.meta.JavaField;
 import jdk.vm.ci.meta.JavaKind;
-import jdk.vm.ci.meta.JavaType;
-import jdk.vm.ci.meta.PrimitiveConstant;
 import jdk.vm.ci.meta.RawConstant;
-import jdk.vm.ci.meta.ResolvedJavaField;
 import org.graalvm.compiler.core.common.type.StampFactory;
 import org.graalvm.compiler.graph.Node;
-import org.graalvm.compiler.graph.iterators.NodeIterable;
-import org.graalvm.compiler.nodes.FixedGuardNode;
-import org.graalvm.compiler.nodes.PiNode;
 import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.nodes.ValuePhiNode;
 import org.graalvm.compiler.nodes.calc.AddNode;
-import org.graalvm.compiler.nodes.calc.IsNullNode;
-import org.graalvm.compiler.nodes.extended.UnboxNode;
-import org.graalvm.compiler.nodes.java.InstanceOfNode;
-import org.graalvm.compiler.nodes.java.LoadFieldNode;
 import org.graalvm.compiler.nodes.java.LoadIndexedNode;
+import org.graalvm.compiler.nodes.java.StoreIndexedNode;
 import org.graalvm.compiler.phases.BasePhase;
-import uk.ac.manchester.tornado.drivers.opencl.graal.nodes.TupleFieldNode;
 import uk.ac.manchester.tornado.runtime.graal.phases.TornadoHighTierContext;
 import org.graalvm.compiler.nodes.ConstantNode;
 
@@ -36,17 +25,16 @@ public class TornadoTupleReplacement extends BasePhase<TornadoHighTierContext> {
     protected void run(StructuredGraph graph, TornadoHighTierContext context) {
         List<Node> prev = new ArrayList<>();
         List<Node> unboxedNodes = new ArrayList<>();
-        // NodeIterable<Node> sucs = null;
+
         ArrayList<ArrayList<Node>> sucs = new ArrayList<ArrayList<Node>>();
         for (Node n : graph.getNodes()) {
             // System.out.println("Node type: " + n.getNodeClass());
             if (n.getNodeClass().toString().contains("LoadFieldNode")) {
+                // get the predecessors of all LoadFieldNodes
+                // this is necessary because we plan to remove the LoadFieldNodes
                 prev.add(n.predecessor());
-                // System.out.println("LoadFieldNode... this has to go");
-                // n.safeDelete();
             } else if (n.getNodeClass().toString().contains("UnboxNode")) {
-                // System.out.println("UnboxNode... this has to go");
-                System.out.println("USAGES of UnBoxNode: ");
+                // get usages of UnboxNode
                 for (Node in : n.usages()) {
                     System.out.println("   " + in.getNodeClass());
                 }
@@ -56,26 +44,6 @@ public class TornadoTupleReplacement extends BasePhase<TornadoHighTierContext> {
                 }
                 sucs.add(ar);
                 unboxedNodes.add(n);
-                // n.safeDelete();
-            }
-        }
-        if (prev != null) {
-            for (Node n : prev) {
-                System.out.println("prev of loadfieldNode " + n.getNodeClass());
-            }
-        }
-
-        if (sucs != null) {
-            for (int i = 0; i < sucs.size(); i++) {
-                ArrayList<Node> ar = sucs.get(i);
-                System.out.println("=== Successors for " + i + " LoadField ");
-                for (Node suc : ar) {
-                    System.out.println("  " + suc.getNodeClass());
-                    // suc.replaceAtPredecessor(prev.get(i));
-                    // System.out.println(" Predecessor of node " + suc.getNodeClass() + " is " +
-                    // suc.predecessor().getNodeClass());
-                    // suc.replaceAndDelete();
-                }
             }
         }
 
@@ -111,49 +79,35 @@ public class TornadoTupleReplacement extends BasePhase<TornadoHighTierContext> {
 
         // ArrayList<Node> loadIndexNodes = new ArrayList<>();
         for (Node n : graph.getNodes()) {
-            // System.out.println("Node type: " + n.getNodeClass());
             if (n.getNodeClass().toString().contains("InstanceOfNode")) {
-                System.out.println("InstanceNode... this has to go");
                 n.safeDelete();
             } else if (n.getNodeClass().toString().contains("FixedGuardNode")) {
-                System.out.println("FixedGuardNode... this has to go");
                 n.safeDelete();
             } else if (n.getNodeClass().toString().contains("PiNode")) {
-                System.out.println("PiNode... this has to go");
                 n.safeDelete();
             } else if (n.getNodeClass().toString().contains("IsNullNode")) {
-                System.out.println("IsNullNode... this has to go");
                 n.safeDelete();
             } else if (n.getNodeClass().toString().contains("UnboxNode")) {
-                System.out.println("UnboxNode... this has to go");
-                int numOfusages = n.getUsageCount();
-                System.out.println("Number of usages of UnboxNode: " + numOfusages);
-                for (int j = 0; j < numOfusages; j++) {
-                    System.out.println("Usage " + j + " : " + n.getUsageAt(j).getNodeClass());
-                }
-                // if (n.predecessor() != null) {
+                // replace at usages the UnboxNode with its predecessor
+                // this is done because moving forward we will remove the UnboxNode
                 Node pred = LoadIndexedNodes.remove();
                 System.out.println("++ Predecessor of UnboxNode " + pred);
                 n.replaceAtUsages(pred);
-                // n.safeDelete();
-                // }
-                // n.replaceAndDelete(sucs.get(j).get(0));
-                // j++;
-                // n.safeDelete();
             }
         }
 
+        // replace in the graph the UnboxNodes with their predecessors
         for (Node n : graph.getNodes()) {
             for (Node suc : n.successors()) {
                 if (suc.getNodeClass().toString().contains("UnboxNode")) {
                     for (Node sucsuc : suc.successors()) {
-                        System.out.println("Replace " + suc.getNodeClass() + " with " + sucsuc.getNodeClass());
                         n.replaceFirstSuccessor(suc, sucsuc);
                     }
                 }
             }
         }
 
+        // Remove the UnboxNodes from the graph
         for (Node n : graph.getNodes()) {
             if (n.getNodeClass().toString().contains("UnboxNode")) {
                 n.safeDelete();
@@ -191,7 +145,7 @@ public class TornadoTupleReplacement extends BasePhase<TornadoHighTierContext> {
         }
 
         // number of bytes to skip
-        Constant cv = new RawConstant(4);
+        Constant cv = new RawConstant(1);
         ConstantNode c = new ConstantNode(cv, StampFactory.positiveInt());
         graph.addOrUnique(c);
         ValuePhiNode ph;
@@ -221,6 +175,35 @@ public class TornadoTupleReplacement extends BasePhase<TornadoHighTierContext> {
                 }
             }
 
+        }
+
+        List<LoadIndexedNode> newlist = new ArrayList<>();
+        for (Node n : graph.getNodes()) {
+            if (n.getNodeClass().toString().contains("LoadIndexedNode")) {
+                LoadIndexedNode cld = (LoadIndexedNode) n;
+                newlist.add(cld);
+                // JavaKind jvk = JavaKind.fromJavaClass(int.class);
+                // LoadIndexedNode ld = new LoadIndexedNode(null, cld.array(), cld.index(),
+                // jvk);
+
+            }
+        }
+
+        for (int k = 0; k < newlist.size(); k++) {
+            LoadIndexedNode cld = newlist.get(k);
+            JavaKind jvk = JavaKind.fromJavaClass(int.class);
+            LoadIndexedNode ld = graph.addOrUnique(new LoadIndexedNode(null, cld.array(), cld.index(), jvk));
+            graph.replaceFixed(cld, ld);
+        }
+
+        for (Node n : graph.getNodes()) {
+            if (n.getNodeClass().toString().contains("StoreIndexed")) {
+                JavaKind jvk = JavaKind.fromJavaClass(int.class);
+                StoreIndexedNode st = (StoreIndexedNode) n;
+                StoreIndexedNode newst = graph.addOrUnique(new StoreIndexedNode(st.array(), st.index(), jvk, st.value()));
+                graph.replaceFixed(st, newst);
+                break;
+            }
         }
 
     }
